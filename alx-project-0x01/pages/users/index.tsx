bb4bda100b27a8de8647b77490df3d4419419d0d
Emnet Befkadu <emnbef456@gmail.com>
2025-09-24 11:58:18 +0300

    user

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,16 +1,49 @@
+// import React from "react";
+// // import UserCard from "@/components/common/UserCard";
+// import { UserProps } from "@/interfaces";
+
+// interface UserCardProps {
+//   user: UserProps;
+// }
+
+// const UserCard: React.FC<UserCardProps> = ({ user }) => {
+//   return (
+//     <div>
+//       <h2>{user.name}</h2>
+//       <p>{user.email}</p>
+//       <span>User ID: {user.id}</span>
+//     </div>
+//   );
+// };
+
+
+// export async function getStaticProps() {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//   const posts = await response.json();
+
+//   return {
+//     props: {
+//       posts
+//     }
+//   };
+// }
+
+// export default UsersCard;
+
+
 import React from "react";
 import UserCard from "@/components/common/UserCard";
 import { UserProps } from "@/interfaces";
 
 interface UsersProps {
-  posts: UserProps[]; // the fetched users
+  users: UserProps[];
 }
 
-const Users: React.FC<UsersProps> = ({ posts }) => {
+const UsersPage: React.FC<UsersProps> = ({ users }) => {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">Users List</h1>
-      {posts.map((user) => (
+      {users.map((user) => (
         <UserCard key={user.id} user={user} />
       ))}
     </div>
@@ -19,13 +52,13 @@ const Users: React.FC<UsersProps> = ({ posts }) => {
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const posts = await response.json();
+  const users: UserProps[] = await response.json();
 
   return {
     props: {
-      posts
-    }
+      users,
+    },
   };
 }
 
-export default Users;
+export default UsersPage;
