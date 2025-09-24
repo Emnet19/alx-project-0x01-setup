b0d2aad5b92a586ba7f27bb6670ba655095e6af6
Emnet Befkadu <emnbef456@gmail.com>
2025-09-24 10:52:04 +0300

    post card

---
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -0,0 +1,7 @@
+const PostCard = () => {
+  return (
+    <div className="border p-4 rounded shadow-md">
+        <h2 className="text-xl font-bold mb-2">Post Title</h2>
+        </div>  
+  )}
+  export default PostCard;                  
