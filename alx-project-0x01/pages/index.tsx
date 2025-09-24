c85e386294191d4ea3e36716cb7a8b45fd0eba84
Emnet Befkadu <emnbef456@gmail.com>
2025-09-24 10:47:37 +0300

    setup

---
+++ alx-project-0x01/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home: React.FC=()=>{
+  return(
+    <div className="flex justify-center items-center h-screen bg-green-900 rounded-md">
+      <h1 className="text-7xl font-thin text-white ">Welcome to the Home Page</h1>
+    </div>
+  )
+}
+export default Home;
