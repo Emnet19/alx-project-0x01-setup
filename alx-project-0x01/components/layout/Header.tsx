605b47ff1123ecf21a83880ce0634257f09d8bb3
Emnet Befkadu <emnbef456@gmail.com>
2025-09-24 11:00:54 +0300

    change

--- alx-project-0x01/components/layout/Header.tsx
+++ alx-project-0x01/components/layout/Header.tsx
@@ -1,7 +1,26 @@
-const Header = () => {
+import Link from "next/link";
+import Posts from "../../pages/posts";
+import Users from "@/pages/users";
+const Header: React.FC = () => {
     return(
-        <div>
+           <header className="bg-blue-600 text-white shadow-md py-4">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <h3 className="font-bold text-2xl">
+     <Link href="/">Daily Contents</Link>
+  </h3>
+        <nav>
+          <ul className="flex space-x-6">
+            <li className="hover:underline">
+              <Link href="/posts">Posts</Link>
+            </li>
+            <li className="hover:underline">
+              <Link href="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    )
+}
 
-        </div>
-    )}
-    export default Header;
+export default Header;
